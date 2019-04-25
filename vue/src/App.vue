25a098b22415224f5c65602e088c83493a817c02
llz <2861399191@qq.com>
2019-04-25 18:49:30 +0800

    vue 学习研究的第一个版本

---
+++ vue/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld :msg="msg" title="这是一个标题"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  data:function(){
+      return {
+        msg:"你好呀"
+      }
+  },
+  name: 'app',
+  created:function(){
+      console.log("created");
+  },
+
+  mounted:function(){
+      console.log("mounted");
+  },
+  updated:function() {
+      console.log("updated");
+  },
+  components: {
+    HelloWorld
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
